ea0123d22f32c23afc815c307b7ecdf4f48d7a8f
paltoalko <martas90909@me.com>
2021-12-02 18:35:02 +0000

    works on screen

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const keys = calculator.querySelector('.calculator__keys')
 const display = calculator.querySelector('.calculator__display')
 
 
+    
 keys.addEventListener('click', event => {
     if (!event.target.closest('button')) return
 
@@ -11,12 +12,11 @@ keys.addEventListener('click', event => {
     const displayValue = display.textContent
     const { type } = key.dataset
     const { previousKeyType } = calculator.dataset
+    
     if (type === 'number') {
-        if (displayValue === '0') {
+        if (displayValue === '0' || previousKeyType === 'operator' ) {
             display.textContent = keyValue
-        } else if (previousKeyType === 'operator') {
-            display.textContent = keyValue;
-        }
+        } 
         else {
             display.textContent = displayValue + keyValue;
         }
@@ -27,12 +27,47 @@ keys.addEventListener('click', event => {
         operatorKeys.forEach(el => { el.dataset.state = ''})
         key.dataset.state = 'selected'
 
-        console.log(key); 
+        calculator.dataset.firstNumber = displayValue
+        calculator.dataset.operator = key.dataset.key
+ 
     }
 
     if (type === 'equal') {
-
+        const firstNumber = calculator.dataset.firstNumber
+        const operator = calculator.dataset.operator
+        const secondNumber = displayValue
+        display.textContent = calculate(firstNumber,operator, secondNumber)
+        removeClass()
+        
     }
-    
+    if (type === 'clear') {
+        display.textContent = "0"
+        removeClass()
+        delete calculator.dataset.firstNumber
+        delete calculator.dataset.operator
+    }
+
+    if (type === 'decimal') {
+        if(!displayValue.includes(keyValue)) {
+            display.textContent += keyValue;
+        }
+    }
+
     calculator.dataset.previousKeyType = type;
 })
+
+function calculate(firstNumber,operator, secondNumber) {
+    firstNumber = parseFloat(firstNumber)
+    secondNumber = parseFloat(secondNumber)
+
+    if (operator === 'plus') return (firstNumber + secondNumber).toFixed(2)
+    if (operator === 'minus') return (firstNumber - secondNumber).toFixed(2)
+    if (operator === 'times') return (firstNumber * secondNumber).toFixed(2)
+    if (operator === 'divide') return (firstNumber / secondNumber).toFixed(2)
+}
+
+
+function removeClass () {
+    let activeClass = keys.querySelectorAll('[data-type="operator"]')
+    activeClass.forEach(el => el.dataset.state = [''])
+}
